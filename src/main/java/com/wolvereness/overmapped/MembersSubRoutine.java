Wolvereness/OverMapped

commit eda0b18f7ee653f81df233e0d94a5b2644060774

Refactor out imperative logic to `SubRoutines'

src/main/java/com/wolvereness/overmapped/MembersSubRoutine.java
@@ -1,0 +1,210 @@
+/*
+ * This file is part of OverMapped.
+ *
+ * OverMapped is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OverMapped is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with OverMapped.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wolvereness.overmapped;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoFailureException;
+import org.objectweb.asm.commons.Remapper;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multimap;
+import com.wolvereness.overmapped.asm.Signature;
+import com.wolvereness.overmapped.asm.Signature.MutableSignature;
+
+class MembersSubRoutine extends SubRoutine {
+
+	MembersSubRoutine() {
+		super("members");
+	}
+
+	@Override
+	public void invoke(
+	                   final OverMapped instance,
+	                   final Multimap<String, String> rdepends,
+	                   final BiMap<String, String> nameMaps,
+	                   final BiMap<String, String> inverseNameMaps,
+	                   final BiMap<Signature, Signature> signatureMaps,
+	                   final BiMap<Signature, Signature> inverseSignatureMaps,
+	                   final Remapper inverseMapper,
+	                   final MutableSignature signature,
+	                   final Set<String> searchCache,
+	                   final Map<Signature, Integer> flags,
+	                   final Map<?,?> map
+	                   ) throws
+	                   ClassCastException,
+	                   NullPointerException,
+	                   MojoFailureException
+	                   {
+		final Object memberMaps = map.get(tag);
+		if (memberMaps instanceof Map)
+			return;
+
+		for (final Map.Entry<?, ?> memberMap : ((Map<?,?>) memberMaps).entrySet()) {
+			final Object key = memberMap.getKey();
+			final Collection<String> classes;
+			final String name;
+			final String newName;
+			final String description;
+
+			if (key instanceof Iterable) {
+				final ImmutableCollection.Builder<String> classesBuilder = ImmutableList.builder();
+				for (final Object clazz : (Iterable<?>) key) {
+					classesBuilder.add(asType(
+						clazz,
+						"`%4$s' contains a %2$s `%1$s', expected a %5$s, from `%3$s'",
+						false,
+						memberMaps,
+						key,
+						String.class
+						));
+				}
+				classes = classesBuilder.build();
+
+				final Map<?,?> information = asType(
+					memberMap.getValue(),
+					"Expected a value `%4$s'->%5$s in %3%s, got %2$s `%1$s'",
+					false,
+					memberMaps,
+					key,
+					Map.class
+					);
+				name = getStringFrom(information, "name", false);
+				newName = getStringFrom(information, "map", false);
+				description = getStringFrom(information, "description", true);
+			} else {
+				final String qualifiedName = asType(
+					key,
+					"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
+					false,
+					memberMaps,
+					memberMap,
+					String.class
+					);
+				final int firstSpace = qualifiedName.indexOf(' ');
+				final int finalSpace = qualifiedName.lastIndexOf(' ');
+
+				if (firstSpace == finalSpace) {
+					if (firstSpace == -1)
+						throw new MojoFailureException(String.format(
+							"Malformed mapping %s",
+							qualifiedName
+							));
+					classes = ImmutableList.of(qualifiedName.substring(0, firstSpace));
+					name = qualifiedName.substring(finalSpace + 1);
+					description = null;
+				} else if (qualifiedName.indexOf(' ', firstSpace + 1) != finalSpace)
+					throw new MojoFailureException(String.format(
+						"Malformed mapping %s",
+						qualifiedName
+						));
+				else {
+					classes = ImmutableList.of(qualifiedName.substring(0, firstSpace));
+					name = qualifiedName.substring(firstSpace + 1, finalSpace);
+					description = qualifiedName.substring(finalSpace + 1);
+				}
+
+				newName = asType(
+					memberMap.getValue(),
+					"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
+					false,
+					memberMaps,
+					memberMap,
+					String.class
+					);
+			}
+
+			if (description == null) {
+				instance.getLog().warn(String.format( // TODO
+					"Wildcard description matching not yet implemented - %s %s->%s",
+					classes,
+					name,
+					newName
+					));
+				continue;
+			}
+
+			final String unmappedDescription = inverseMapper.mapDesc(description);
+
+			for (final String clazz : classes) {
+				final String original = inverseNameMaps.get(clazz);
+				if (original == null) {
+					instance.missingAction.actMember(instance.getLog(), clazz, name, newName, description, inverseSignatureMaps);
+					continue;
+				}
+
+				if (!updateMember(instance, searchCache, signatureMaps, inverseSignatureMaps, signature, name, newName, description, unmappedDescription, clazz, original)) {
+					continue;
+				}
+
+				if (signature.isMethod()) {
+					for (final String inherited : rdepends.get(original)) {
+						updateMember(instance, searchCache, signatureMaps, inverseSignatureMaps, signature, name, newName, description, unmappedDescription, nameMaps.get(inherited), inherited);
+					}
+				}
+			}
+
+			searchCache.clear();
+		}
+	}
+
+
+	private boolean updateMember(
+	                             final OverMapped instance,
+	                             final Set<String> cache,
+	                             final BiMap<Signature, Signature> signatureMaps,
+	                             final BiMap<Signature, Signature> inverseSignatureMaps,
+	                             final Signature.MutableSignature signature,
+	                             final String name,
+	                             final String newName,
+	                             final String description,
+	                             final String unmappedDescription,
+	                             final String clazz,
+	                             final String original
+	                             ) throws
+	                             MojoFailureException
+	                             {
+		if (!cache.add(original))
+			return false;
+
+		signature.update(original, name, unmappedDescription);
+
+		final Signature originalSignature = inverseSignatureMaps.get(signature);
+		if (originalSignature != null) {
+			try {
+				signatureMaps.put(originalSignature, signature.forElementName(newName));
+			} catch (final IllegalArgumentException ex) {
+				final MojoFailureException mojoEx = new MojoFailureException(String.format(
+					"Cannot map %s (currently %s) to pre-existing member %s (in class %s)",
+					originalSignature,
+					signature,
+					signature.forElementName(newName),
+					clazz
+					));
+				mojoEx.initCause(ex);
+				throw mojoEx;
+			}
+		} else {
+			instance.missingAction.actMember(instance.getLog(), clazz, name, newName, description, inverseSignatureMaps);
+		}
+		return true;
+	}
+}

src/main/java/com/wolvereness/overmapped/ClassesSubRoutine.java
@@ -1,0 +1,76 @@
+/*
+ * This file is part of OverMapped.
+ *
+ * OverMapped is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OverMapped is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with OverMapped.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wolvereness.overmapped;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoFailureException;
+import org.objectweb.asm.commons.Remapper;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.Multimap;
+import com.wolvereness.overmapped.asm.Signature;
+import com.wolvereness.overmapped.asm.Signature.MutableSignature;
+
+class ClassesSubRoutine extends SubRoutine {
+
+	ClassesSubRoutine() {
+		super("classes");
+	}
+
+	@Override
+	public void invoke(
+	                   final OverMapped instance,
+	                   final Multimap<String, String> rdepends,
+	                   final BiMap<String, String> nameMaps,
+	                   final BiMap<String, String> inverseNameMaps,
+	                   final BiMap<Signature, Signature> signatureMaps,
+	                   final BiMap<Signature, Signature> inverseSignatureMaps,
+	                   final Remapper inverseMapper,
+	                   final MutableSignature signature,
+	                   final Set<String> searchCache,
+	                   final Map<Signature, Integer> flags,
+	                   final Map<?,?> map
+	                   ) throws
+	                   ClassCastException,
+	                   NullPointerException,
+	                   MojoFailureException
+	                   {
+		final Object classMaps = map.get(tag);
+		if (!(classMaps instanceof Map))
+			return;
+
+		for (final Map.Entry<?, ?> classMap : ((Map<?,?>) classMaps).entrySet()) {
+			final String originalName = ((String) classMap.getKey()).toString();
+			final String newName = ((String) classMap.getValue()).toString();
+			if (nameMaps.containsValue(newName))
+				throw new MojoFailureException(String.format(
+					"Cannot map `%s' to a duplicate entry `%s' mapped from `%s'",
+					originalName,
+					newName,
+					inverseNameMaps.get(newName)
+					));
+			final String trueOriginal = inverseNameMaps.get(originalName);
+			if (trueOriginal == null) {
+				instance.missingAction.actClass(instance.getLog(), originalName, newName, inverseNameMaps);
+			} else {
+				nameMaps.put(trueOriginal, newName);
+			}
+		}
+	}
+}

src/main/java/com/wolvereness/overmapped/FlagsSubRoutine.java
@@ -1,0 +1,106 @@
+/*
+ * This file is part of OverMapped.
+ *
+ * OverMapped is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OverMapped is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with OverMapped.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wolvereness.overmapped;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.maven.plugin.MojoFailureException;
+import org.objectweb.asm.commons.Remapper;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.Multimap;
+import com.wolvereness.overmapped.asm.Signature;
+import com.wolvereness.overmapped.asm.Signature.MutableSignature;
+
+class FlagsSubRoutine extends SubRoutine {
+
+	FlagsSubRoutine() {
+		super("flags");
+	}
+
+	@Override
+	public void invoke(
+	                   final OverMapped instance,
+	                   final Multimap<String, String> rdepends,
+	                   final BiMap<String, String> nameMaps,
+	                   final BiMap<String, String> inverseNameMaps,
+	                   final BiMap<Signature, Signature> signatureMaps,
+	                   final BiMap<Signature, Signature> inverseSignatureMaps,
+	                   final Remapper inverseMapper,
+	                   final MutableSignature signature,
+	                   final Set<String> searchCache,
+	                   final Map<Signature, Integer> flags,
+	                   final Map<?,?> map
+	                   ) throws
+	                   ClassCastException,
+	                   NullPointerException,
+	                   MojoFailureException
+	                   {
+		final Object flagMaps = map.get(tag);
+		if (!(flagMaps instanceof Map))
+			return;
+
+		for (final Map.Entry<?, ?> flagMap : ((Map<?,?>) flagMaps).entrySet()) {
+			final String qualifiedName = SubRoutine.asType(
+				flagMap.getKey(),
+				"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
+				false,
+				flagMaps,
+				flagMap,
+				String.class
+				);
+			final Integer flag = SubRoutine.asType(
+				flagMap.getValue(),
+				"Expected a value `%4$s'->%5$s in %3%s, got %2$s `%1$s'",
+				false,
+				flagMaps,
+				qualifiedName,
+				Integer.class
+				);
+
+
+			final int firstSpace = qualifiedName.indexOf(' ');
+			final int finalSpace = qualifiedName.lastIndexOf(' ');
+
+			if (firstSpace == finalSpace || qualifiedName.indexOf(' ', firstSpace + 1) != finalSpace)
+				throw new MojoFailureException(String.format(
+					"Malformed mapping %s",
+					qualifiedName
+					));
+
+			final String clazz = qualifiedName.substring(0, firstSpace);
+			final String name = qualifiedName.substring(firstSpace + 1, finalSpace);
+			final String description = qualifiedName.substring(finalSpace + 1);
+
+			final String unmappedClass = inverseNameMaps.get(clazz);
+			if (unmappedClass == null) {
+				instance.missingAction.actFlag(instance.getLog(), flagMap, signatureMaps);
+				continue;
+			}
+			final String unmappedDescription = inverseMapper.mapDesc(description);
+
+			final Signature unmappedSignature = inverseSignatureMaps.get(signature.update(unmappedClass, name, unmappedDescription));
+			if (unmappedSignature == null) {
+				instance.missingAction.actFlag(instance.getLog(), flagMap, signatureMaps);
+				continue;
+			}
+
+			flags.put(unmappedSignature, flag);
+		}
+	}
+}

src/main/java/com/wolvereness/overmapped/OverMapped.java
@@ -55,7 +55,6 @@ import org.yaml.snakeyaml.Yaml;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
@@ -89,7 +88,7 @@ public class OverMapped extends AbstractMojo implements UncaughtExceptionHandler
 
 	@Parameter(defaultValue="WARN", required=true, property="mapping.missing")
 	private String missing;
-	private Missing missingAction = Missing.WARN;
+	Missing missingAction = Missing.WARN;
 
 	private volatile Pair<Thread, Throwable> uncaught;
 
@@ -121,7 +120,7 @@ public class OverMapped extends AbstractMojo implements UncaughtExceptionHandler
 		}
 	}
 
-	public void process() throws Throwable {
+	private void process() throws Throwable {
 		validateInput();
 
 		final MultiProcessor executor = MultiProcessor.newMultiProcessor(cores - 1, new ThreadFactoryBuilder().setDaemon(true).setNameFormat(OverMapped.class.getName() + "-processor-%d").setUncaughtExceptionHandler(this).build());
@@ -204,203 +203,27 @@ public class OverMapped extends AbstractMojo implements UncaughtExceptionHandler
 		for (final Object mapping : mappings.get()) {
 			final Map<?,?> map = (Map<?, ?>) mapping;
 
-			try {
-				final Object classMaps = map.get("classes");
-				if (classMaps instanceof Map) {
-					for (final Map.Entry<?, ?> classMap : ((Map<?,?>) classMaps).entrySet()) {
-						final String originalName = ((String) classMap.getKey()).toString();
-						final String newName = ((String) classMap.getValue()).toString();
-						if (nameMaps.containsValue(newName))
-							throw new MojoFailureException(String.format(
-								"Cannot map `%s' to a duplicate entry `%s' mapped from `%s'",
-								originalName,
-								newName,
-								inverseNameMaps.get(newName)
-								));
-						final String trueOriginal = inverseNameMaps.get(originalName);
-						if (trueOriginal == null) {
-							missingAction.actClass(getLog(), originalName, newName, inverseNameMaps);
-						} else {
-							nameMaps.put(trueOriginal, newName);
-						}
-					}
+			for (final SubRoutine subRoutine : SubRoutine.SUB_ROUTINES) {
+				try {
+					subRoutine.invoke(
+						this,
+						rdepends,
+						nameMaps,
+						inverseNameMaps,
+						signatureMaps,
+						inverseSignatureMaps,
+						inverseMapper,
+						signature,
+						searchCache,
+						flags,
+						map
+						);
+				} catch (final Exception ex) {
+					final Throwable throwable = new MojoFailureException(
+						"Failed to parse mappings in " + mapping);
+					throwable.initCause(ex);
+					throw throwable;
 				}
-			} catch (final Exception ex) {
-				final Throwable throwable = new MojoFailureException("Failed to parse class mappings in " + mapping);
-				throwable.initCause(ex);
-				throw throwable;
-			}
-
-			try {
-				final Object memberMaps = map.get("members");
-				if (memberMaps instanceof Map) {
-					for (final Map.Entry<?, ?> memberMap : ((Map<?,?>) memberMaps).entrySet()) {
-						final Object key = memberMap.getKey();
-						final Collection<String> classes;
-						final String name;
-						final String newName;
-						final String description;
-
-						if (key instanceof Iterable) {
-							final ImmutableCollection.Builder<String> classesBuilder = ImmutableList.builder();
-							for (final Object clazz : (Iterable<?>) key) {
-								classesBuilder.add(asType(
-									clazz,
-									"`%4$s' contains a %2$s `%1$s', expected a %5$s, from `%3$s'",
-									false,
-									memberMaps,
-									key,
-									String.class
-									));
-							}
-							classes = classesBuilder.build();
-
-							final Map<?,?> information = asType(
-								memberMap.getValue(),
-								"Expected a value `%4$s'->%5$s in %3%s, got %2$s `%1$s'",
-								false,
-								memberMaps,
-								key,
-								Map.class
-								);
-							name = getStringFrom(information, "name", false);
-							newName = getStringFrom(information, "map", false);
-							description = getStringFrom(information, "description", true);
-						} else {
-							final String qualifiedName = asType(
-								key,
-								"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
-								false,
-								memberMaps,
-								memberMap,
-								String.class
-								);
-							final int firstSpace = qualifiedName.indexOf(' ');
-							final int finalSpace = qualifiedName.lastIndexOf(' ');
-
-							if (firstSpace == finalSpace) {
-								if (firstSpace == -1)
-									throw new MojoFailureException(String.format(
-										"Malformed mapping %s",
-										qualifiedName
-										));
-								classes = ImmutableList.of(qualifiedName.substring(0, firstSpace));
-								name = qualifiedName.substring(finalSpace + 1);
-								description = null;
-							} else if (qualifiedName.indexOf(' ', firstSpace + 1) != finalSpace)
-								throw new MojoFailureException(String.format(
-									"Malformed mapping %s",
-									qualifiedName
-									));
-							else {
-								classes = ImmutableList.of(qualifiedName.substring(0, firstSpace));
-								name = qualifiedName.substring(firstSpace + 1, finalSpace);
-								description = qualifiedName.substring(finalSpace + 1);
-							}
-
-							newName = asType(
-								memberMap.getValue(),
-								"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
-								false,
-								memberMaps,
-								memberMap,
-								String.class
-								);
-						}
-
-						if (description == null) {
-							getLog().warn(String.format( // TODO
-								"Wildcard description matching not yet implemented - %s %s->%s",
-								classes,
-								name,
-								newName
-								));
-							continue;
-						}
-
-						final String unmappedDescription = inverseMapper.mapDesc(description);
-
-						for (final String clazz : classes) {
-							final String original = inverseNameMaps.get(clazz);
-							if (original == null) {
-								missingAction.actMember(getLog(), clazz, name, newName, description, inverseSignatureMaps);
-								continue;
-							}
-
-							if (!updateMember(searchCache, signatureMaps, inverseSignatureMaps, signature, name, newName, description, unmappedDescription, clazz, original)) {
-								continue;
-							}
-
-							if (signature.isMethod()) {
-								for (final String inherited : rdepends.get(original)) {
-									updateMember(searchCache, signatureMaps, inverseSignatureMaps, signature, name, newName, description, unmappedDescription, nameMaps.get(inherited), inherited);
-								}
-							}
-						}
-
-						searchCache.clear();
-					}
-				}
-			} catch (final Exception ex) {
-				final Throwable throwable = new MojoFailureException("Failed to parse member mappings in " + mapping);
-				throwable.initCause(ex);
-				throw throwable;
-			}
-
-			try {
-				final Object flagMaps = map.get("flags");
-				if (flagMaps instanceof Map) {
-					for (final Map.Entry<?, ?> flagMap : ((Map<?,?>) flagMaps).entrySet()) {
-						final String qualifiedName = asType(
-							flagMap.getKey(),
-							"`%4$s' points from a %2$s `%1$s', expected a %5$s, in `%3$s'",
-							false,
-							flagMaps,
-							flagMap,
-							String.class
-							);
-						final Integer flag = asType(
-							flagMap.getValue(),
-							"Expected a value `%4$s'->%5$s in %3%s, got %2$s `%1$s'",
-							false,
-							flagMaps,
-							qualifiedName,
-							Integer.class
-							);
-
-
-						final int firstSpace = qualifiedName.indexOf(' ');
-						final int finalSpace = qualifiedName.lastIndexOf(' ');
-
-						if (firstSpace == finalSpace || qualifiedName.indexOf(' ', firstSpace + 1) != finalSpace) throw new MojoFailureException(String.format(
-							"Malformed mapping %s",
-							qualifiedName
-							));
-
-						final String clazz = qualifiedName.substring(0, firstSpace);
-						final String name = qualifiedName.substring(firstSpace + 1, finalSpace);
-						final String description = qualifiedName.substring(finalSpace + 1);
-
-						final String unmappedClass = inverseNameMaps.get(clazz);
-						if (unmappedClass == null) {
-							missingAction.actFlag(getLog(), flagMap, signatureMaps);
-							continue;
-						}
-						final String unmappedDescription = inverseMapper.mapDesc(description);
-
-						final Signature unmappedSignature = inverseSignatureMaps.get(signature.update(unmappedClass, name, unmappedDescription));
-						if (unmappedSignature == null) {
-							missingAction.actFlag(getLog(), flagMap, signatureMaps);
-							continue;
-						}
-
-						flags.put(unmappedSignature, flag);
-					}
-				}
-			} catch (final Exception ex) {
-				final Throwable throwable = new MojoFailureException("Failed to parse class mappings in " + mapping);
-				throwable.initCause(ex);
-				throw throwable;
 			}
 		}
 
@@ -476,103 +299,6 @@ public class OverMapped extends AbstractMojo implements UncaughtExceptionHandler
 		}
 	}
 
-	private boolean updateMember(
-	                             final Set<String> cache,
-	                             final BiMap<Signature, Signature> signatureMaps,
-	                             final BiMap<Signature, Signature> inverseSignatureMaps,
-	                             final Signature.MutableSignature signature,
-	                             final String name,
-	                             final String newName,
-	                             final String description,
-	                             final String unmappedDescription,
-	                             final String clazz,
-	                             final String original
-	                             ) throws
-	                             MojoFailureException
-	                             {
-		if (!cache.add(original))
-			return false;
-
-		signature.update(original, name, unmappedDescription);
-
-		final Signature originalSignature = inverseSignatureMaps.get(signature);
-		if (originalSignature != null) {
-			try {
-				signatureMaps.put(originalSignature, signature.forElementName(newName));
-			} catch (final IllegalArgumentException ex) {
-				final MojoFailureException mojoEx = new MojoFailureException(String.format(
-					"Cannot map %s (currently %s) to pre-existing member %s (in class %s)",
-					originalSignature,
-					signature,
-					signature.forElementName(newName),
-					clazz
-					));
-				mojoEx.initCause(ex);
-				throw mojoEx;
-			}
-		} else {
-			missingAction.actMember(getLog(), clazz, name, newName, description, inverseSignatureMaps);
-		}
-		return true;
-	}
-
-	private String getStringFrom(
-	                             final Map<?,?> map,
-	                             final String token,
-	                             final boolean acceptNull
-	                             ) throws
-	                             ClassCastException,
-	                             NullPointerException
-	                             {
-		return getTypeFrom(map, token, acceptNull, String.class);
-	}
-
-	private <T> T getTypeFrom(
-	                          final Map<?,?> map,
-	                          final String token,
-	                          final boolean acceptNull,
-	                          final Class<T> clazz
-	                          ) throws
-	                          ClassCastException,
-	                          NullPointerException
-	                          {
-		return asType(
-			map.get(token),
-			"Expected a token \"%4$s\"->%5$s in `%3s', got %2$s `%1$s'",
-			acceptNull,
-			map,
-			token,
-			clazz
-			);
-	}
-
-	private <T> T asType(
-	                     final Object value,
-	                     final String message,
-	                     final boolean acceptNull,
-	                     final Object source,
-	                     final Object identifier,
-	                     final Class<T> clazz
-	                     ) throws
-	                     ClassCastException,
-	                     NullPointerException
-	                     {
-		if (clazz.isInstance(value))
-			return clazz.cast(value);
-		if (acceptNull && value == null)
-			return null;
-		final String exceptionMessage = String.format(
-			message,
-			value,
-			value == null ? Object.class : value.getClass(),
-			source,
-			identifier,
-			clazz
-			);
-		if (value == null)
-			throw new NullPointerException(exceptionMessage);
-		throw new ClassCastException(exceptionMessage);
-	}
 
 	private void validateInput() throws MojoExecutionException, MojoFailureException {
 		if (cores <=0)

commit 27d2b9bf202ac215442ec4c0e5ceee7f8cb51cf5

Fix inverted input validation.
eda0b18f7ee653f81df233e0d94a5b2644060774 introduced a bug that caused the
members' entry validation to be inverted; it would ClassCastException poor
input while ignoring proper input.

src/main/java/com/wolvereness/overmapped/MembersSubRoutine.java
@@ -55,7 +55,7 @@ class MembersSubRoutine extends SubRoutine {
 	                   MojoFailureException
 	                   {
 		final Object memberMaps = map.get(tag);
-		if (memberMaps instanceof Map)
+		if (!(memberMaps instanceof Map))
 			return;
 
 		for (final Map.Entry<?, ?> memberMap : ((Map<?,?>) memberMaps).entrySet()) {